c82015b7ef1260cba747512e960580c70b7e9b53
tiagopinheirorocha <161031251+tiagopinheirorocha@users.noreply.github.com>
2024-03-22 11:10:15 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const listaNomes = document.body.querySelector('#listaNomes');
+    const inputNome = document.body.querySelector('#inputNome'); 
+    const buttonAdicionar = document.body.querySelector('#buttonAdicionar');
+    buttonAdicionar.addEventListener('click', () => {
+        const div = document.createElement('div');
+        div.innerHTML = '<li>'+inputNome.value+'</li>';
+        listaNomes.appendChild(div.firstElementChild);
 
+
+
+    })
+
+
+       
+});
